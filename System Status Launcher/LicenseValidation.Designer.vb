ef76ab797a300ea5ab16a643f3ccce75506a62d5
GeoCyberwolf <geostar@outlook.com>
2019-08-19 20:14:54 +0100

    Began to add liicensing. Trying out Habanero licensing

---
+++ System Status Launcher/LicenseValidation.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LicenseValidation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxLicenseKey = New System.Windows.Forms.TextBox()
+        Me.LabelEnterKey = New System.Windows.Forms.Label()
+        Me.ButtonTrial = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxLicenseKey
+        '
+        Me.TextBoxLicenseKey.Location = New System.Drawing.Point(46, 50)
+        Me.TextBoxLicenseKey.Multiline = True
+        Me.TextBoxLicenseKey.Name = "TextBoxLicenseKey"
+        Me.TextBoxLicenseKey.Size = New System.Drawing.Size(469, 103)
+        Me.TextBoxLicenseKey.TabIndex = 0
+        '
+        'LabelEnterKey
+        '
+        Me.LabelEnterKey.AutoSize = True
+        Me.LabelEnterKey.Location = New System.Drawing.Point(46, 31)
+        Me.LabelEnterKey.Name = "LabelEnterKey"
+        Me.LabelEnterKey.Size = New System.Drawing.Size(93, 13)
+        Me.LabelEnterKey.TabIndex = 1
+        Me.LabelEnterKey.Text = "Enter License Key"
+        '
+        'ButtonTrial
+        '
+        Me.ButtonTrial.Location = New System.Drawing.Point(46, 230)
+        Me.ButtonTrial.Name = "ButtonTrial"
+        Me.ButtonTrial.Size = New System.Drawing.Size(121, 23)
+        Me.ButtonTrial.TabIndex = 2
+        Me.ButtonTrial.Text = "Activate 30-Day Trial"
+        Me.ButtonTrial.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(379, 230)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(136, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Activate Full License Key"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LicenseValidation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(587, 265)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ButtonTrial)
+        Me.Controls.Add(Me.LabelEnterKey)
+        Me.Controls.Add(Me.TextBoxLicenseKey)
+        Me.Name = "LicenseValidation"
+        Me.Text = "License Validation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxLicenseKey As TextBox
+    Friend WithEvents LabelEnterKey As Label
+    Friend WithEvents ButtonTrial As Button
+    Friend WithEvents Button2 As Button
+End Class
--- System Status Launcher/Main.vb
+++ System Status Launcher/Main.vb
@@ -1,5 +1,74 @@
 ﻿Imports System.Text.RegularExpressions
+Imports Habanero.Licensing.Validation
 Public Class Main
+
+    '///LICENSING CODE///
+    'create code for applicationsecret
+    Dim applicationSecret() As Byte = Convert.FromBase64String("c3ZoNKo2mUG1wswVtsHPDQ==")
+    'create code for public key
+    Dim publicKey() As Byte = Convert.FromBase64String("BgIAAACkAABSU0ExAAIAAAEAAQA9uPZd4CiX3PM4ha6kGnhE1EhQqwpraGdlMzZWBEM0L20jG8zpnRnR6IpDXLyyzzBdPEVOk0WFgBsjQQ2O7C/q")
+
+    Private ReadOnly Property Validator As Habanero.Licensing.Validation.LicenseValidator
+        Get
+            'this version is for file system - Isolated storage is anther option
+            Return New Habanero.Licensing.Validation.LicenseValidator(Habanero.Licensing.Validation.LicenseLocation.File, "Path to license file", "IPO Toolbox", publicKey, applicationSecret, ThisVersion)
+        End Get
+    End Property
+
+    Private Shared ReadOnly Property ThisVersion As Version
+        Get
+            'Get the executing files filesversion
+            Dim fileversion As FileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location)
+            Return New Version(fileversion.FileMajorPart, fileversion.FileMinorPart, fileversion.FileBuildPart, fileversion.FilePrivatePart)
+
+        End Get
+    End Property
+
+    Private Sub DoLicenseCheck()
+        Dim result As Habanero.Licensing.Validation.LicenseValidationResult = Validator.CheckLicense()
+        If (result.State = Habanero.Licensing.Validation.LicenseState.Invalid) Then
+            If (result.Issues.Contains(LicenseIssue.NoLicenseInfo)) Then
+                'inform user there is no license info
+                MsgBox(Prompt:="No valid license found!")
+            Else
+                If (result.Issues.Contains(LicenseIssue.ExpiredDateSoft)) Then
+                    'inform user that their license has expired but
+                    'that they may continue using the software for a period
+                    MsgBox(Prompt:="License expired1")
+                End If
+                If (result.Issues.Contains(LicenseIssue.ExpiredDateHard)) Then
+                    'inform user that their license has expired
+                    MsgBox(Prompt:="License expired2")
+                End If
+                If (result.Issues.Contains(LicenseIssue.ExpiredVersion)) Then
+                    'inform user that their license is for an earlier version
+                    MsgBox(Prompt:="License expired3")
+                End If
+                'other messages
+            End If
+
+            'prompt user for trial or to insert license info then decide what to do
+            'activate trial
+            result = Validator.ActivateTrial(45)
+            'LicenseValidation.Show()
+            'Me.Close()
+            'or save license
+            Dim userLicense As String = "Get the license string from your user"
+            result = Validator.CheckLicense(userLicense)
+            'decide if you want to save the license...
+            Validator.SaveLicense(userLicense)
+        End If
+        If (result.State = LicenseState.Trial) Then
+            'activate trial features
+        End If
+        If (result.State = LicenseState.Valid) Then
+            'activate product
+            If (Validator.IsEdition("Pro")) Then
+                'activate pro features...
+            End If
+        End If
+    End Sub
+    '////END LICENSING CODE////
 
     'Define global variables
     Dim systemlist As New DataTable
@@ -9,6 +78,8 @@ Public Class Main
     Dim keyCombo As New List(Of Keys)({Keys.Shift, Keys.Enter})
     Dim currentKeys As New List(Of Keys)
     Dim DarkMode As Boolean
+    Dim CSVLineCount As Integer = 0
+    Dim MaxCSVSize As Integer = 50
     'Function to launch system status with our IP, Username and Password
     Public Sub LaunchSSA()
         Try
@@ -32,7 +103,8 @@ Public Class Main
             systemlist.Rows.Clear()
             Dim csvreader As New IO.StreamReader(My.Settings.CSVPath, System.Text.Encoding.Default)
             Dim csvline As String = ""
-            Do
+            Do 'Until CSVLineCount = MaxCSVSize
+                CSVLineCount = CSVLineCount + 1
                 csvline = csvreader.ReadLine
                 If csvline Is Nothing Then Exit Do
                 Dim thecolumns() As String = csvline.Split(",")
@@ -149,6 +221,7 @@ Public Class Main
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DoLicenseCheck()
         Me.KeyPreview = True
 
         If My.Settings.UpgradeRequired = True Then
