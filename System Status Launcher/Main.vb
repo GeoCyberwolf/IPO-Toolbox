1a4e444ae0327bb84603b430202989daa789495c
George Thinkpad Yoga <contact@geosmith.uk>
2019-08-22 12:54:39 +0100

    Moved licensing functions to Licensing.vb

--- System Status Launcher/Main.vb
+++ System Status Launcher/Main.vb
@@ -1,76 +1,8 @@
 ﻿Imports System.Text.RegularExpressions
-Imports Habanero.Licensing.Validation
+Imports IPO_Toolbox.Licensing
 Imports IPO_Toolbox.Functions
 Imports IPO_Toolbox.GlobalVars
 Public Class Main
-    '///LICENSING CODE///
-    'create code for applicationsecret
-    Dim applicationSecret() As Byte = Convert.FromBase64String("c3ZoNKo2mUG1wswVtsHPDQ==")
-    'create code for public key
-    Dim publicKey() As Byte = Convert.FromBase64String("BgIAAACkAABSU0ExAAIAAAEAAQA9uPZd4CiX3PM4ha6kGnhE1EhQqwpraGdlMzZWBEM0L20jG8zpnRnR6IpDXLyyzzBdPEVOk0WFgBsjQQ2O7C/q")
-
-    Private ReadOnly Property Validator As Habanero.Licensing.Validation.LicenseValidator
-        Get
-            'this version is for file system - Isolated storage is anther option
-            Return New Habanero.Licensing.Validation.LicenseValidator(Habanero.Licensing.Validation.LicenseLocation.File, "Path to license file", "IPO Toolbox", publicKey, applicationSecret, ThisVersion)
-        End Get
-    End Property
-
-    Private Shared ReadOnly Property ThisVersion As Version
-        Get
-            'Get the executing files filesversion
-            Dim fileversion As FileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location)
-            Return New Version(fileversion.FileMajorPart, fileversion.FileMinorPart, fileversion.FileBuildPart, fileversion.FilePrivatePart)
-
-        End Get
-    End Property
-
-    Private Sub DoLicenseCheck()
-        Dim result As Habanero.Licensing.Validation.LicenseValidationResult = Validator.CheckLicense()
-        If (result.State = Habanero.Licensing.Validation.LicenseState.Invalid) Then
-            If (result.Issues.Contains(LicenseIssue.NoLicenseInfo)) Then
-                'inform user there is no license info
-                MsgBox(Prompt:="No valid license found!")
-            Else
-                If (result.Issues.Contains(LicenseIssue.ExpiredDateSoft)) Then
-                    'inform user that their license has expired but
-                    'that they may continue using the software for a period
-                    MsgBox(Prompt:="License expired1")
-                End If
-                If (result.Issues.Contains(LicenseIssue.ExpiredDateHard)) Then
-                    'inform user that their license has expired
-                    MsgBox(Prompt:="License expired2")
-                End If
-                If (result.Issues.Contains(LicenseIssue.ExpiredVersion)) Then
-                    'inform user that their license is for an earlier version
-                    MsgBox(Prompt:="License expired3")
-                End If
-                'other messages
-            End If
-
-            'prompt user for trial or to insert license info then decide what to do
-            'activate trial
-            result = Validator.ActivateTrial(45)
-            Me.Enabled = False
-            Me.Hide()
-            LicensePrompt.Show()
-            'or save license
-            Dim userLicense As String = ""
-            result = Validator.CheckLicense(userLicense)
-            'decide if you want to save the license...
-            Validator.SaveLicense(userLicense)
-        End If
-        If (result.State = LicenseState.Trial) Then
-            'activate trial features
-        End If
-        If (result.State = LicenseState.Valid) Then
-            'activate product
-            If (Validator.IsEdition("Pro")) Then
-                'activate pro features...
-            End If
-        End If
-    End Sub
-    '////END LICENSING CODE////
 
     Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
         OpenFromTray()
