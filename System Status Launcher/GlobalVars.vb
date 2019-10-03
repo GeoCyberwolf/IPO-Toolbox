81277470b58f2876f85a2ed14da845c6201a8a44
George Smith <contact@geosmith.uk>
2019-10-03 16:40:07 +0100

    Changed manager path so it works on 32 and 64 bit windows

--- System Status Launcher/GlobalVars.vb
+++ System Status Launcher/GlobalVars.vb
@@ -9,8 +9,8 @@
     Public Shared DarkMode As Boolean
     Public Shared CSVLineCount As Integer = 0
     Public Shared MaxCSVSize As Integer
-    Public Shared ManagerPath As String = "C:\Program Files (x86)\Avaya\IP Office\Manager\Manager.exe"
     Public Shared programfiles As String = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+    Public Shared ManagerPath As String = programfiles & "\Avaya\IP Office\Manager\Manager.exe"
     Public Shared username As String
     Public Shared password As String
     Public Shared LicenseKey As String
